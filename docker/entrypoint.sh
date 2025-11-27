c9ffe9dec96515ab56688e6335e6ea5f44bc1456
Ivan David Martinez Baron <ivandavid.martinez10@gmail.com>
2025-11-27 15:38:55 -0500

    Wip on issue 31. Changed docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 # Continue with the container startup
 exec gosu isaac-sim "$@"
 source /opt/ros/humble/setup.bash
-bash
+exec "$@"
--- docker/run.sh
+++ docker/run.sh
@@ -13,14 +13,18 @@ declare SCRIPT_NAME=$(readlink -f ${BASH_SOURCE[0]})
 cd $(dirname $SCRIPT_NAME)
 
 BUILD=""
+BUILD_KIT=""
 
 if [[ ! -z "$1" ]]; then
     if [[ "$1" == "--build" || "$1" == "-b" ]]; then
         BUILD="--build"
+    elif [[ "$1" == "--buildkit" || "$1" == "-k" ]]; then
+        BUILD="--build"
+        BUILD_KIT="DOCKER_BUILDKIT=1"    
     else
         echo "Unknown argument ${1}"
         exit 1
     fi
 fi
 
-LOCAL_UID=$(id -u) LOCAL_GID=$(id -g) docker compose run ${BUILD} --rm --remove-orphans andino_isaac
+LOCAL_UID=$(id -u) LOCAL_GID=$(id -g) env ${BUILD_KIT} docker compose run ${BUILD} --rm --remove-orphans andino_isaac
